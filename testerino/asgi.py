b58ce08270b112193019ce921bf2380b3b86aa6a
Lucas Watterson <lucas.watterson@gmail.com>
2023-03-20 20:10:48 +0000

    Update Project Name

--- testerino/asgi.py
+++ testerino/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for djazure project.
+ASGI config for testerino project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djazure.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "testerino.settings")
 
 application = get_asgi_application()
--- testerino/urls.py
+++ testerino/urls.py
@@ -1,4 +1,4 @@
-"""djazure URL Configuration
+"""testerino URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
